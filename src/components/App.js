b82a51cf5e3e641ef8b5e19b167b2992341304bc
Luis Bryan Dominguez Ramirez <luisbdominguezr@Luiss-MBP.attlocal.net>
2021-11-12 13:57:33 -0500

    baisc routing done

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,16 @@ const App = () => {
       <RouteContainer>
         <Route exact path="/">
           <Login/>
-        </Route>          
+        </Route>  
+        <Route exact path="/login">
+          <Login/>
+        </Route> 
+        <Route exact path="/view">
+          <View/>
+        </Route>    
+        <Route exact path="/logout">
+          <Logout/>
+        </Route>                                 
       </RouteContainer>
     </AppContainer>
   )
